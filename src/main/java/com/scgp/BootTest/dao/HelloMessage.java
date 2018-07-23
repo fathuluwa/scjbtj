e00a4010fb762af04c86f7452737d8a9b0b6dea5
ahxzb <471017728@qq.com>
2018-07-23 10:37:25 +0800

    gqy提交测试

--- src/main/java/com/scgp/BootTest/dao/HelloMessage.java
+++ src/main/java/com/scgp/BootTest/dao/HelloMessage.java
@@ -3,12 +3,19 @@ package com.scgp.BootTest.dao;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.log4j.Log4j;
 
+/**
+ * @ProjectName: BootTest
+ * @Package: com.scgp.BootTest
+ * @ClassName: HelloMessage
+ * @Description: java类作用描述
+ * @Author: Bush
+ * @CreateDate: 2018/6/15 11:48
+ * @Version: 1.0
+ */
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-
+@AllArgsConstructor
 public class HelloMessage {
     private String name;
 }
